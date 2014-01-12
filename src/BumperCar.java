9c236e3a940f5ac152c0ccc4ad3313b685f00476
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-01-12 09:56:41 -0600

    Implements basic functionality for the car moving forward and stopping.

--- src/BumperCar.java
+++ src/BumperCar.java
@@ -1,12 +1,46 @@
+import lejos.hardware.motor.Motor;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
 /**
  * Main class of the BumperClass project.
  */
 
 public class BumperCar
 {
+    private static RegulatedMotor motorR = Motor.A;
+    private static RegulatedMotor motorL = Motor.D;
+
+
     public static void main(String[] args)
     {
-        log("Hello, World");
+        log("Starting Program");
+
+        forward();
+
+        Delay.msDelay(4000);
+
+        stop();
+
+        log("Program Ends");
+    }
+
+
+    private static void stop()          // Stops both motors to stop the rover
+    {
+        log("STOP");
+
+        motorR.stop();
+        motorL.stop();
+    }
+
+
+    private static void forward()
+    {
+        log("FORWARD");
+
+        motorR.forward();
+        motorL.forward();
     }
 
 
