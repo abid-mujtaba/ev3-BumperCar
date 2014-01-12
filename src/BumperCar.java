0e54493585da7ecaf19f6855d432551c8b0013cb
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-01-12 09:36:11 -0600

    Skeletal BumperCar project created.

---
+++ src/BumperCar.java
@@ -0,0 +1,17 @@
+/**
+ * Main class of the BumperClass project.
+ */
+
+public class BumperCar
+{
+    public static void main(String[] args)
+    {
+        log("Hello, World");
+    }
+
+
+    private static void log(String message)
+    {
+        System.out.println("log>\t" + message);
+    }
+}
